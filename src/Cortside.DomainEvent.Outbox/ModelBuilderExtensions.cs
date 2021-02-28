c1827012e2dd38b4ee8724639fe69984887ed453
Cort Schaefer <cort@eschaefer.net>
2021-02-27 18:48:45 -0700

    add explicit modelbuilder information for output entity; update nuget package inforamtion

--- src/Cortside.DomainEvent.Outbox/ModelBuilderExtensions.cs
+++ src/Cortside.DomainEvent.Outbox/ModelBuilderExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -10,6 +11,39 @@ namespace Cortside.DomainEvent.EntityFramework {
 
     internal class OutboxMessageEntityConfiguration : IEntityTypeConfiguration<Outbox> {
         public void Configure(EntityTypeBuilder<Outbox> builder) {
+            builder.ToTable("Outbox");
+            builder.HasKey(t => t.MessageId);
+
+            builder.Property(t => t.MessageId)
+                .HasMaxLength(36)
+                .IsRequired()
+                .ValueGeneratedNever();
+
+            builder.Property(t => t.CorrelationId)
+                .HasMaxLength(36);
+
+            builder.Property(t => t.EventType)
+                .HasMaxLength(250)
+                .IsRequired();
+
+            builder.Property(t => t.Address)
+                .HasMaxLength(250)
+                .IsRequired();
+
+            builder.Property(t => t.Body)
+                .IsRequired();
+
+            builder.Property(t => t.Status)
+                .HasMaxLength(10)
+                .IsRequired()
+                .HasConversion(
+                    v => v.ToString(),
+                    v => (OutboxStatus)Enum.Parse(typeof(OutboxStatus), v)
+                );
+
+            builder.Property(t => t.LockId)
+                    .HasMaxLength(36);
+
             builder.HasIndex(p => new { p.ScheduledDate, p.Status })
                 .IncludeProperties(p => new { p.EventType })
                 .HasName("IX_ScheduleDate_Status");
