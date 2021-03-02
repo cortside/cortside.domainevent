d06fa1edc56786f897c1ea966bda7eff02eeb8cf
Cort Schaefer <cort@eschaefer.net>
2021-03-02 00:51:20 -0700

    add hosted service test

---
+++ src/Cortside.DomainEvent.Tests/ReceiverHostedServiceTest.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Moq;
+using Xunit;
+
+namespace Cortside.DomainEvent.Tests {
+    public class ReceiverHostedServiceTest {
+        [Fact]
+        public async Task Verify_Hosted_Service_Executes_Task() {
+            IServiceCollection services = new ServiceCollection();
+            services.AddLogging();
+            services.AddHostedService<ReceiverHostedService>();
+
+            var receiver = new Mock<IDomainEventReceiver>();
+            receiver.Setup(x => x.StartAndListen(It.IsAny<IDictionary<string, Type>>()));
+            services.AddSingleton<IDomainEventReceiver>(receiver.Object);
+            services.AddSingleton(new ReceiverHostedServiceSettings() { Enabled = true, MessageTypes = new Dictionary<string, Type>() });
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var service = serviceProvider.GetService<IHostedService>() as ReceiverHostedService;
+
+            CancellationTokenSource source = new CancellationTokenSource();
+            await service.StartAsync(source.Token);
+
+            await Task.Delay(1000);
+            receiver.VerifyAll();
+
+            source.Cancel();
+            await service.StopAsync(source.Token);
+        }
+    }
+}
