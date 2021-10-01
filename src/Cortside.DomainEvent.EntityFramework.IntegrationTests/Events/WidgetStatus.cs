85d7e3ee38565c2f3bf58e1f56b59f2616ebeef6
Cort Schaefer <cort@eschaefer.net>
2021-10-01 16:42:25 -0600

    user configured serilizer settings if set

---
+++ src/Cortside.DomainEvent.EntityFramework.IntegrationTests/Events/WidgetStatus.cs
@@ -0,0 +1,7 @@
+namespace Cortside.DomainEvent.EntityFramework.IntegrationTests.Events {
+    public enum WidgetStatus {
+        CreatedWidget,
+        UpdatedWidget,
+        DeletedWidget
+    }
+}
