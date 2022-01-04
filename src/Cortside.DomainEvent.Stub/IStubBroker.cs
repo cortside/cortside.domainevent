7364cf8d9eb7fe101e05868be1f433c7639d4e1f
Cort Schaefer <cort@eschaefer.net>
2022-01-03 17:03:19 -0700

    expose some additional information about messages published and accepted as well as their current contents

--- src/Cortside.DomainEvent.Stub/IStubBroker.cs
+++ src/Cortside.DomainEvent.Stub/IStubBroker.cs
@@ -1,14 +1,24 @@
+using System.Collections.ObjectModel;
 using Amqp;
 
 namespace Cortside.DomainEvent.Stub {
     public interface IStubBroker {
         bool HasItems { get; }
         bool HasDeadLetterItems { get; }
-        void Accept(Message message);
+
+        ReadOnlyCollection<Message> Items { get; }
+        ReadOnlyCollection<Message> DeadLetterItems { get; }
+
+        public int Published { get; }
+        public int Accepted { get; }
+
         void Enqueue(Message message);
         Message Peek();
+
+        void Accept(Message message);
         void Reject(Message message);
         void Release(Message message);
+
         void Shovel();
     }
 }
--- src/Cortside.DomainEvent.Stub/QueueBroker.cs
+++ src/Cortside.DomainEvent.Stub/QueueBroker.cs
@@ -1,13 +1,23 @@
 using System.Collections.Concurrent;
+using System.Collections.ObjectModel;
+using System.Linq;
 using Amqp;
 
 namespace Cortside.DomainEvent.Stub {
     public class QueueBroker : IStubBroker {
         private readonly ConcurrentQueue<Message> queue = new ConcurrentQueue<Message>();
         private readonly ConcurrentQueue<Message> dlq = new ConcurrentQueue<Message>();
+        private int published = 0;
+        private int accepted = 0;
 
         public bool HasItems { get => queue.Count > 0; }
         public bool HasDeadLetterItems { get => dlq.Count > 0; }
+
+        public ReadOnlyCollection<Message> Items { get => queue.ToArray().ToList().AsReadOnly(); }
+        public ReadOnlyCollection<Message> DeadLetterItems { get => dlq.ToArray().ToList().AsReadOnly(); }
+
+        public int Published { get => published; }
+        public int Accepted { get => accepted; }
 
         public Message Peek() {
             Message message;
@@ -23,10 +33,12 @@ namespace Cortside.DomainEvent.Stub {
 
         public void Accept(Message message) {
             queue.TryDequeue(out _);
+            accepted++;
         }
 
         public void Enqueue(Message message) {
             queue.Enqueue(message);
+            published++;
         }
 
         public void Release(Message message) {
