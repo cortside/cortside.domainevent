21e2af5cef0fe817081d07196666f4b0b368a7ab
Cort Schaefer <cort@eschaefer.net>
2022-05-26 09:37:57 -0600

    trying to get coverage to work in sonarqube

--- b.ps1
+++ b.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 dotnet tool install --global dotnet-sonarscanner --version 5.5.3
 dotnet sonarscanner begin /o:"cortside" /k:"cortside_cortside.domainevent" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="88c88c857e5710d2a8ec472b7b5c2e91eb79fc4d" /d:sonar.cs.opencover.reportsPaths="**/*.opencover.xml"
 dotnet build src --configuration Debug /property:Version=1.1.174
-dotnet test src --no-restore --no-build /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+dotnet test src --no-restore --no-build --collect:"XPlat Code Coverage" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=opencover /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 dotnet sonarscanner end /d:sonar.login="88c88c857e5710d2a8ec472b7b5c2e91eb79fc4d"
 
 gci -Path . -File -Recurse *.xml | Select Name
