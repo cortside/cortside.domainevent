20ad16f7032af9dd0b9bac755710690198d5901e
= <=>
2025-02-07 17:55:46 -0700

    clean up

--- src/Cortside.DomainEvent/ServiceCollectionExtensions.cs
+++ src/Cortside.DomainEvent/ServiceCollectionExtensions.cs
@@ -5,7 +5,6 @@ using Cortside.DomainEvent.Handlers;
 using Cortside.DomainEvent.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -52,21 +51,13 @@ namespace Cortside.DomainEvent {
             Guard.From.Null(options, nameof(options));
             Guard.From.Null(options.ReceiverSettings, nameof(options.ReceiverSettings));
             Guard.From.Null(options.HostedServiceSettings, nameof(options.HostedServiceSettings));
-
-            services.AddSingleton(options.ReceiverSettings);
+            Guard.From.NullOrWhitespace(options.ReceiverSettings.Key, nameof(options.ReceiverSettings.Key));
+            Guard.From.NullOrWhitespace(options.ReceiverSettings.Server, nameof(options.ReceiverSettings.Server));
+            Guard.From.NullOrWhitespace(options.ReceiverSettings.Username, nameof(options.ReceiverSettings.Username));
+            Guard.From.NullOrWhitespace(options.ReceiverSettings.Password, nameof(options.ReceiverSettings.Password));
 
             // register and setup settings for handlers
             options.HostedServiceSettings.MessageTypes = new Dictionary<string, Type>();
-            //foreach (var handler in options.Handlers) {
-            //    var handlerType = typeof(IDomainEventHandler<>).MakeGenericType(handler.Value.Event);
-            //    services.AddTransient(handlerType, handler.Value.Handler);
-            //    options.HostedServiceSettings.MessageTypes.Add(handler.Key, handler.Value.Event);
-            //}
-
-            //// Register Hosted Services
-            //services.AddSingleton(options.HostedServiceSettings);
-            //services.AddSingleton<IDomainEventReceiver, DomainEventReceiver>();
-            //services.AddHostedService<ReceiverHostedService>();
             foreach (var handler in options.Handlers) {
                 var handlerType = typeof(IDomainEventHandler<>).MakeGenericType(handler.Value.Event);
                 services.AddTransient(handlerType, handler.Value.Handler);
@@ -74,19 +65,17 @@ namespace Cortside.DomainEvent {
             }
 
             // Register Hosted Services
+            //services.AddKeyedSingleton(options.ReceiverSettings.Key, options.ReceiverSettings); // do we actually need to do this if we use ctor with specific settings??
             //services.AddKeyedSingleton(options.ReceiverSettings.Key, options.HostedServiceSettings); // needed??
             services.AddKeyedSingleton(options.ReceiverSettings.Key, (sp, IDomainEventReceiver) => {
                 var loggerFactory = sp.GetService<ILoggerFactory>();
-                var receiver = new DomainEventReceiver(options.ReceiverSettings, sp, loggerFactory.CreateLogger<DomainEventReceiver>());
-                return receiver;
+                return new DomainEventReceiver(options.ReceiverSettings, sp, loggerFactory.CreateLogger<DomainEventReceiver>());
             });
 
-            services.AddHostedService<ReceiverHostedService>((sp) => {
+            // workaround to AddHostedService extension: https://github.com/dotnet/runtime/issues/38751#issuecomment-1967830195
+            services.AddSingleton<IHostedService, ReceiverHostedService>(sp => {
                 var loggerFactory = sp.GetService<ILoggerFactory>();
-                //var receiver = sp.GetKeyedService<IDomainEventReceiver>(options.ReceiverSettings.Key);
-                var service = new ReceiverHostedService(loggerFactory.CreateLogger<ReceiverHostedService>(), sp, options.HostedServiceSettings, options.ReceiverSettings);
-
-                return service;
+                return new ReceiverHostedService(loggerFactory.CreateLogger<ReceiverHostedService>(), sp, options.HostedServiceSettings, options.ReceiverSettings);
             });
 
             return services;
@@ -100,59 +89,13 @@ namespace Cortside.DomainEvent {
                 var index = optionsList.IndexOf(options);
                 options.HostedServiceSettings = configuration.GetSection($"DomainEvent:Connections:{index}:ReceiverHostedService").Get<ReceiverHostedServiceSettings>();
                 options.ReceiverSettings = configuration.GetSection($"DomainEvent:Connections:{index}").Get<KeyedDomainEventReceiverSettings>();
+                Guard.From.NullOrWhitespace(options.ReceiverSettings.Key, nameof(options.ReceiverSettings.Key));
                 // tech debt
                 options.ReceiverSettings.Service = configuration[$"DomainEvent:Connections:{index}:Key"];
                 options.ReceiverSettings.AppName = configuration[$"DomainEvent:Connections:{index}:Key"];
-                Guard.From.NullOrWhitespace(options.ReceiverSettings.Key, nameof(options.ReceiverSettings.Key));
 
-                //services.AddKeyedSingleton(options.ReceiverSettings.Key, options.ReceiverSettings); // do we actually need to do this if we use ctor with specific settings??
-
-                // register and setup settings for handlers
-                options.HostedServiceSettings.MessageTypes = new Dictionary<string, Type>();
-                foreach (var handler in options.Handlers) {
-                    var handlerType = typeof(IDomainEventHandler<>).MakeGenericType(handler.Value.Event);
-                    services.AddTransient(handlerType, handler.Value.Handler);
-                    options.HostedServiceSettings.MessageTypes.Add(handler.Key, handler.Value.Event);
-                }
-
-                // Register Hosted Services
-                //services.AddKeyedSingleton(options.ReceiverSettings.Key, options.HostedServiceSettings); // needed??
-                services.AddKeyedSingleton(options.ReceiverSettings.Key, (sp, IDomainEventReceiver) => {
-                    var loggerFactory = sp.GetService<ILoggerFactory>();
-                    var receiver = new DomainEventReceiver(options.ReceiverSettings, sp, loggerFactory.CreateLogger<DomainEventReceiver>());
-                    return receiver;
-                });
-
-                // doesn't work, only one gets registered
-                //services.AddHostedService((sp) => {
-                //    var loggerFactory = sp.GetService<ILoggerFactory>();
-                //    //var receiver = sp.GetKeyedService<IDomainEventReceiver>(options.ReceiverSettings.Key);
-                //    return new ReceiverHostedService(loggerFactory.CreateLogger<ReceiverHostedService>(), sp, options.HostedServiceSettings, options.ReceiverSettings);
-                //});
-
-                // workaround to AddHostedService extension: https://github.com/dotnet/runtime/issues/38751#issuecomment-1967830195
-                services.AddSingleton<IHostedService, ReceiverHostedService>(sp => {
-                    var loggerFactory = sp.GetService<ILoggerFactory>();
-                    return new ReceiverHostedService(loggerFactory.CreateLogger<ReceiverHostedService>(), sp, options.HostedServiceSettings, options.ReceiverSettings);
-                });
-
-                // does not work - both get registered but code underneath does not resolve them and start them up per usual... possibly start them explicitly another way??
-                //services.AddKeyedHostedService(options.ReceiverSettings.Key, (sp, ReceiverHostedService) => {
-                //    var loggerFactory = sp.GetService<ILoggerFactory>();
-                //    //var receiver = sp.GetKeyedService<IDomainEventReceiver>(options.ReceiverSettings.Key);
-                //    var service = new ReceiverHostedService(loggerFactory.CreateLogger<ReceiverHostedService>(), sp, options.HostedServiceSettings, options.ReceiverSettings);
-
-                //    return service;
-                //});
+                services = services.AddKeyedDomainEventReceiver(options);
             }
-
-
-            return services;
-        }
-
-        public static IServiceCollection AddKeyedHostedService<THostedService>(this IServiceCollection services, object? serviceKey, Func<IServiceProvider, object?, THostedService> implementationFactory)
-            where THostedService : class, IHostedService {
-            services.TryAddEnumerable(ServiceDescriptor.KeyedSingleton<IHostedService>(serviceKey, implementationFactory));
 
             return services;
         }
